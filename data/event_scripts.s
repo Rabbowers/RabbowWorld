a3b63070be184e51c7a73c5880d26d0d18705416
Cornelius Andreas Rosenaa <75324436+coffandro@users.noreply.github.com>
2023-08-02 11:24:05 +0200

    Added the LD of Seaton Town, and the ship

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1055,3 +1055,11 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/SeatonTown/scripts.inc"
+
+	.include "data/maps/InsideOfShip/scripts.inc"
+
+	.include "data/maps/JoltikCave/scripts.inc"
+
+	.include "data/maps/Route1/scripts.inc"
